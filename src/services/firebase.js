9da3b12825e674c9052f02a60d61e2af72c30163
RyanEAA <aparicioryan2003@gmail.com>
2026-06-02 01:24:15 -0500

    updated file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,18 +3,6 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // get .env file values
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDYB2OOLsVOkpyoSK7BqilziyVOOdpSHYM",
-//   authDomain: "shots-73521.firebaseapp.com",
-//   projectId: "shots-73521",
-//   storageBucket: "shots-73521.firebasestorage.app",
-//   messagingSenderId: "64374401687",
-//   appId: "1:64374401687:web:acb3540dee52991ccc17d2",
-//   measurementId: "G-9FMMYY3FEC"
-// };
-console.log(import.meta.env);
-console.log(import.meta.env.apiKey);
-console.log("hi");
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
